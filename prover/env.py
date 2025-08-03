27e522a60f117e39daf5a0285318f5b8058b6555
pufanyi <FPU001@e.ntu.edu.sg>
2025-08-03 23:58:21 +0800

    Update configuration files and improve code formatting
    - Added linting configuration to `pyproject.toml` for Ruff.
    - Refactored `env.py` and `lean_runner.py` for improved readability and consistency.
    - Updated `.gitignore` to maintain clarity on Visual Studio Code settings.
    - Minor adjustments in `install.md` for consistency.
    - Ensured newline at the end of files where necessary.

--- prover/env.py
+++ prover/env.py
@@ -1,13 +1,17 @@
 from pathlib import Path
+
 import yaml
 from pydantic import BaseModel
+
 
 class LeanConfig(BaseModel):
     executable: str
     workspace: str
 
+
 class Config(BaseModel):
     lean: LeanConfig
+
 
 with open(Path(__file__).parents[1] / "config.yaml", "r") as f:
     config_dict = yaml.safe_load(f)
--- prover/lean_runner.py
+++ prover/lean_runner.py
@@ -1,23 +1,35 @@
-import subprocess
 import json
+import subprocess
+
 
 class Proof(object):
     def __init__(self, proof: str):
         self.proof = proof
         self.lean_workspace = "playground"
-        self.command = json.dumps({
-            "cmd": self.proof,
-            "allTactics": False,
-            "ast": False, "tactics": False, "premises": False
-        })
-    
+        self.command = json.dumps(
+            {
+                "cmd": self.proof,
+                "allTactics": False,
+                "ast": False,
+                "tactics": False,
+                "premises": False,
+            }
+        )
+
     def execute(self):
-        outputs = subprocess.run(["/root/.elan/bin/lake", "exe", 'repl'], input=self.command, capture_output=True, text=True, cwd="/root/playground")
+        outputs = subprocess.run(
+            ["/root/.elan/bin/lake", "exe", "repl"],
+            input=self.command,
+            capture_output=True,
+            text=True,
+            cwd="/root/playground",
+        )
         return {
             "stdout": outputs.stdout,
             "stderr": outputs.stderr,
             "returncode": outputs.returncode,
         }
+
 
 if __name__ == "__main__":
     with open("test.lean", "r") as f:
