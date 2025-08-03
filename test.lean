8c6537239dc66db93c7990f59038c954c11a4257
BIGKnight <yezhen.wang0305@gmail.com>
2025-08-03 22:06:41 +0800

    Refactor `test.lean` to adjust the order of open statements, placing `BigOperators` first for improved clarity.

--- test.lean
+++ test.lean
@@ -3,7 +3,7 @@ import Aesop
 
 set_option maxHeartbeats 0
 
-open Real Nat Topology Rat BigOperators
+open BigOperators Real Nat Topology Rat
 
 /-- The volume of a cone is given by the formula $V = \frac{1}{3}Bh$, where $B$ is the area of the base and $h$ is the height. The area of the base of a cone is 30 square units, and its height is 6.5 units. What is the number of cubic units in its volume? Show that it is 65.-/
 theorem mathd_algebra_478 (b h v : ℝ) (h₀ : 0 < b ∧ 0 < h ∧ 0 < v) (h₁ : v = 1 / 3 * (b * h))
