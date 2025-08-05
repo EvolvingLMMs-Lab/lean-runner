7ea1153ec36596e8f6ece13db87e3d7ff68614c8
Fanyi Pu <FPU001@e.ntu.edu.sg>
2025-08-05 18:29:58 +0800

    update

--- packages/client/lean_client/__init__.py
+++ packages/client/lean_client/__init__.py
@@ -1,4 +1,4 @@
-from .client.async_client import AsyncLeanClient
+from .client.aio.client import AsyncLeanClient
 from .client.client import LeanClient
 
 __all__ = ["LeanClient", "AsyncLeanClient"]
