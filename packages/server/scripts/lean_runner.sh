b215bfa6f4a6e232cf567678a662ed4615e989dd
pufanyi <FPU001@e.ntu.edu.sg>
2025-08-06 15:07:15 +0800

    Remove obsolete script files for Docker commands to streamline server setup

--- packages/server/scripts/lean_runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-COMMAND='cd /workspace/lean-server/ && source .venv/bin/activate && lean-server'
-
-docker exec -e PYTHONUNBUFFERED=1 -e FORCE_COLOR=1 -e TERM=xterm-256color lean-server zsh -c "${COMMAND}"
