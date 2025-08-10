c0a683c06699cea91b952ab0681e197349cd0f1a
Fanyi Pu <FPU001@e.ntu.edu.sg>
2025-08-10 21:55:59 +0800

    Add Sphinx themes for improved documentation styling
    - Added 'pydata-sphinx-theme' and 'sphinx-material' as development dependencies in pyproject.toml.
    - Updated Sphinx configuration in both client and server documentation to use 'pydata_sphinx_theme' for enhanced visual presentation.

--- sphinx_docs/client/source/conf.py
+++ sphinx_docs/client/source/conf.py
@@ -35,7 +35,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+# html_theme = "furo"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
 
 # This is the critical setting for deploying to a subdirectory
