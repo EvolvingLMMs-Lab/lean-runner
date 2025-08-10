3261f4bbf404ab8165e55d089bef794520131be1
Fanyi Pu <FPU001@e.ntu.edu.sg>
2025-08-10 18:48:59 +0800

    Refactor Sphinx configuration for consistency
    - Updated string formatting in `conf.py` to use double quotes for consistency.
    - Cleaned up the import path and project information section for improved readability.

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -11,33 +11,32 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'lmms-lean-runner'
-copyright = '2024, lmms-lean-runner'
-author = 'lmms-lean-runner'
+project = "lmms-lean-runner"
+author = "lmms-lean-runner"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 # This is the critical setting for deploying to a subdirectory
-html_baseurl = '/docs/'
+html_baseurl = "/docs/"
